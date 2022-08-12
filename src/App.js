ae07372e3ef112cca52824213907e32f16cb3581
Joy Serquiña <stepjoyous@gmail.com>
2022-08-12 10:47:50 -0700

    Created basic circle/wheel style. Added food list and successfully mapped through list to generate list of foods.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [selectedFood, setSelectedFood] = useState('')
+
+  const foodList = [
+    'Sushi',
+    'Pizza',
+    'Burger & Fries',
+    'Wings',
+    'Dumplings/Dim Sum',
+    'Hot Pot',
+    'Korean BBQ',
+    'Vietnamese',
+    'Poke',
+    'Hawaiian',
+    'Noods'
+  ]
+
+  useEffect(() => {
+    console.log(foodList)
+  }, []) 
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          What's to eat?
+        </h1>
+        <section className="spinner-section">
+          <div>
+            <ul className="circle">
+              {foodList.map((foodGroup, index) => (<li 
+                key={index}
+                className="spinner-slice">
+                <div 
+                  className="text"
+                  spellCheck="false">
+                  <p>{foodGroup}</p>
+                </div>
+              </li>))}
+            </ul>
+          </div>
+        </section>
       </header>
     </div>
   );
