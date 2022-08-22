338ae12db9c0fa01eef25384e53324b83c075d7e
Joy Serquiña <stepjoyous@gmail.com>
2022-08-21 22:32:57 -0700

    Added mobx npm package for future implementation. Also updated logic on Spinner to use separate foodlist and color array not the object with linked food items and slice colors so that we can change the value for different users.

--- src/components/spinner/Spinner.jsx
+++ src/components/spinner/Spinner.jsx
@@ -14,71 +14,72 @@ const Spinner = () => {
 
   const spinTime = Math.floor((Math.random() * 10000) + 1)
 
-  const foodObjectList = [
-    {
-      name: 'Sushi',
-      bgColor: 'rgba(255, 195, 18, 1)'
-    },
-    {
-      name: 'Pizza',
-      bgColor: 'rgba(247, 159, 31, 1)'
-    },
-    {
-      name: 'Burgers',
-      bgColor: 'rgba(238, 90, 36, 1)'
-    },
-    {
-      name: 'Wings',
-      bgColor: 'rgba(181, 52, 113, 1)'
-    },
-    {
-      name: 'Dim Sum',
-      bgColor: 'rgba(131, 52, 113, 1)'
-    },
-    {
-      name: 'Hot Pot',
-      bgColor: 'rgba(153, 128, 250, 1)'
-    },
-    {
-      name: 'Thai',
-      bgColor: 'rgba(217, 128, 250, 1)'
-    },
-    {
-      name: 'Korean BBQ',
-      bgColor: 'rgba(18, 203, 196, 1)'
-    },
-    {
-      name: 'Vietnamese',
-      bgColor: 'rgba(18, 137, 167, 1)'
-    },
-    {
-      name: 'Poke',
-      bgColor: 'rgba(6, 82, 221, 1)'
-    },
-    {
-      name: 'Mexican',
-      bgColor: 'rgba(0, 148, 50, 1)'
-    },
-    {
-      name: 'Hawaiian',
-      bgColor: 'rgba(163, 203, 56, 1)'
-    },
-  ]
+  // const foodObjectList = [
+  //   {
+  //     name: 'Sushi',
+  //     bgColor: 'rgba(255, 195, 18, 1)'
+  //   },
+  //   {
+  //     name: 'Pizza',
+  //     bgColor: 'rgba(247, 159, 31, 1)'
+  //   },
+  //   {
+  //     name: 'Burgers',
+  //     bgColor: 'rgba(238, 90, 36, 1)'
+  //   },
+  //   {
+  //     name: 'Wings',
+  //     bgColor: 'rgba(181, 52, 113, 1)'
+  //   },
+  //   {
+  //     name: 'Dim Sum',
+  //     bgColor: 'rgba(131, 52, 113, 1)'
+  //   },
+  //   {
+  //     name: 'Hot Pot',
+  //     bgColor: 'rgba(153, 128, 250, 1)'
+  //   },
+  //   {
+  //     name: 'Thai',
+  //     bgColor: 'rgba(217, 128, 250, 1)'
+  //   },
+  //   {
+  //     name: 'Korean BBQ',
+  //     bgColor: 'rgba(18, 203, 196, 1)'
+  //   },
+  //   {
+  //     name: 'Vietnamese',
+  //     bgColor: 'rgba(18, 137, 167, 1)'
+  //   },
+  //   {
+  //     name: 'Poke',
+  //     bgColor: 'rgba(6, 82, 221, 1)'
+  //   },
+  //   {
+  //     name: 'Mexican',
+  //     bgColor: 'rgba(0, 148, 50, 1)'
+  //   },
+  //   {
+  //     name: 'Hawaiian',
+  //     bgColor: 'rgba(163, 203, 56, 1)'
+  //   },
+  // ]
 
 // Next feature: Make Food list editable by others
-  // const foodList = [
-  //   'Sushi',
-  //   'Pizza',
-  //   'Burger & Fries',
-  //   'Wings',
-  //   'Dumplings/Dim Sum',
-  //   'Hot Pot',
-  //   'Korean BBQ',
-  //   'Vietnamese',
-  //   'Poke',
-  //   'Hawaiian',
-  //   'Noods'
-  // ]
+  const foodList = [
+    'Sushi',
+    'Pizza',
+    'Burgers',
+    'Wings',
+    'Dim Sum',
+    'Hot Pot',
+    'Korean BBQ',
+    'Vietnamese',
+    'Poke',
+    'Hawaiian',
+    'Mexican',
+    'Thai'
+  ]
 
   const pieColors = [
     '#FFC312',
@@ -93,8 +94,7 @@ const Spinner = () => {
     '#0652DD',
     '#006266',
     '#009432',
-    '#A3CB38',
-    '#C4E538'
+    '#A3CB38'
   ]
 
   // useEffect(() => {
@@ -111,7 +111,7 @@ const Spinner = () => {
       <div className="spinner-wheel">
         <div className="arrow-pointer"></div>
         <ul className={spinStart.name}>
-          {foodObjectList.map((food, index) => (<li 
+          {foodList.map((item, index) => (<li 
             key={index}
             className="spinner-slice"
             style={{
@@ -120,7 +120,7 @@ const Spinner = () => {
             <div 
               className="text"
               spellCheck="false">
-              {food.name}
+              {foodList[index]}
             </div>
           </li>))}
         </ul>
